b57ff4ff6897a45abb738398a3a9086de960a29f
Vishwagna R <vishwagnar@gmail.com>
2023-03-07 19:40:52 +0530

    used ElectricEngine in vehicle class

--- Vehicle.java
+++ Vehicle.java
@@ -1,6 +1,11 @@
 public class Vehicle {
 	
 	public static void main(String[] args) {
+		
+		ElectricEngine e = new ElectricEngine();
+		
+		e.start();
+		e.stop();
 		System.out.println("Hello");
 	}
 }
