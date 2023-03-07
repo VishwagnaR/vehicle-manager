1525195a62997e29afa0dad871a2433639844561
Vishwagna R <vishwagnar@gmail.com>
2023-03-07 19:36:38 +0530

    stop method added

--- ElectricEngine.java
+++ ElectricEngine.java
@@ -1,5 +1,9 @@
 public class ElectricEngine {
 	public void start(){
-		System.out.println("Electric vehicle started")
+		System.out.println("Electric vehicle started");
+		
+		public void stop() {
+			System.out.println("Vehicle Stop");
+		}
 	}
 }
