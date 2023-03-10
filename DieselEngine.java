2ec97c387920d281143917a8a23e4f8b50ad89f2
Vishwagna R <vishwagnar@gmail.com>
2023-03-10 19:43:59 +0530

    Added power and fuel

--- DieselEngine.java
+++ DieselEngine.java
@@ -1,10 +1,14 @@
 public class DieselEngine {
 	
+	public void start() {
+		System.out.println("Diesel engine started");
+	}
 	public void generatepower() {
-		System.out.println("Diesel engine generating power")
+		System.out.println("Diesel engine generating power");
 	}
 	
 	public void addingFuel() {
-		
+		System.out.println("Diesel engine generating power");
+
 	}
 }
